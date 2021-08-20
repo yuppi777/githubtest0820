40f46b7773ca45e133b913838a69ffeb40d8136a
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2021-08-20 13:20:04 +0900

    スクリプトに変更を加えた

--- Assets/testscript.cs
+++ Assets/testscript.cs
@@ -7,7 +7,7 @@ public class testscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("aaaaaa");
+        Debug.Log("abcd");
     }
 
     // Update is called once per frame
