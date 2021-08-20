5e3aa5a46b76e16eb0c20fc3c4e032c763ca2688
yuppi777 <88840972+yuppi777@users.noreply.github.com>
2021-08-20 13:32:37 +0900

    間違え

--- Assets/testscript.cs
+++ Assets/testscript.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class testscript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject Cube;
     void Start()
     {
         Debug.Log("abcd");
+        Instantiate(Cube, new Vector3(-1.0f, 0.0f, 0.0f), Quaternion.identity);
+
     }
 
     // Update is called once per frame
